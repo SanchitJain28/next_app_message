b2b2f7fe70a737b6f24be2e31168663147d912a8
SanchitJain28 <mythichuman28@gmail.com>
2025-02-16 19:56:58 +0530

    error update 6

--- src/context/Authentication.tsx
+++ src/context/Authentication.tsx
@@ -2,31 +2,32 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { Message } from '@/model/User'
 export type loginDetails = {
-  email:string
-  isAccepting:boolean
-  username:string
+  email: string
+  isAccepting: boolean
+  username: string
   checkDetails: (token: string) => Promise<void>; // Add this line to the interface
 }
-export interface AuthContextType  {
+export interface AuthContextType {
   loginDetails?: loginDetails | null; // Adjust based on your data
-  setMessages: React.Dispatch<React.SetStateAction<Message[]|[]>> // Adjust based on your function
-  messages?: Message[] |[]; // Adjust based on your data
+  setMessages: React.Dispatch<React.SetStateAction<Message[] | []>> // Adjust based on your function
+  messages?: Message[] | []; // Adjust based on your data
   setLoginDetails?: React.Dispatch<React.SetStateAction<loginDetails | null>>
 }
-export const authContext = createContext<AuthContextType|null>(null)
+export const authContext = createContext<AuthContextType | null>(null)
 
 export function Authentication({ children }: { children: React.ReactNode }) {
-  const [loginDetails, setLoginDetails] = useState<loginDetails|null>(null)
+  const [loginDetails, setLoginDetails] = useState<loginDetails | null>(null)
   const [messages, setMessages] = useState<Message[]>([])
   useEffect(() => {
     // const loginDetails = JSON.parse(localStorage.getItem("loginDetails") ?? "")
-   const loginDetailsinLocalStorage = JSON.parse(localStorage.getItem("loginDetails")??"")
-   if(loginDetailsinLocalStorage!==""){
-     setLoginDetails(loginDetailsinLocalStorage)
-   }
-   else{
-    setLoginDetails(null)
-   }
+    const loginDetailsinLocalStorage = localStorage.getItem("loginDetails") 
+    if (loginDetailsinLocalStorage) {
+      const parsedLoginDetails=JSON.parse(loginDetailsinLocalStorage)
+      setLoginDetails(parsedLoginDetails)
+    }
+    else {
+      setLoginDetails(null)
+    }
   }, [])
 
   // const checkDetails = async (token: string) => {
